86a2179d54b80163c3ac8789d538f737a61c1af7
migue <migue@MIGUELLITO>
2025-10-22 19:45:34 -0500

    Implementación del patrón Proxy y auditoría en creación de usuarios

---
+++ backend/webapicsharp/Interface/Servicios/Abstracciones/IServicioAuditoria.cs
@@ -0,0 +1,7 @@
+﻿namespace webapicsharp.Servicios.Abstracciones
+{
+    public interface IServicioAuditoria
+    {
+        Task RegistrarAccionAsync(string Accion, string? Usuario = null);
+    }
+}
---
+++ backend/webapicsharp/Servicios/ServicioUsuarioProxy.cs
@@ -0,0 +1,26 @@
+﻿
+using webapicsharp.Servicios.Abstracciones;
+
+namespace webapicsharp.Servicios
+{
+    public class ServicioUsuarioProxy : IServicioUsuario
+    {
+        private readonly IServicioUsuario _ServicioReal;
+        private readonly IServicioAuditoria _auditoria;
+
+        public ServicioUsuarioProxy(IServicioUsuario ServicioReal, IServicioAuditoria auditoria)
+        {
+            _ServicioReal = ServicioReal;   
+            _auditoria = auditoria;  
+        }
+
+        public async Task<int> CrearUsuarioAsync(string Nombre, string Cedula, string Correo, string Direccion, string Telefono, string Contraseña)
+        {
+            await _auditoria.RegistrarAccionAsync($"Intento de creación de usuario: {Nombre}");
+            var resultado = await _ServicioReal.CrearUsuarioAsync(Nombre, Cedula, Correo, Direccion, Telefono, Contraseña);
+            await _auditoria.RegistrarAccionAsync($"Usuario '{Nombre}' creado correctamente con ID {resultado}");
+
+            return resultado;
+        }
+    }
+}
