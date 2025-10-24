a2f94ae93ce72d91ecbf680a29d3677cae8517f9
JaiderValencia <janvame05@gmail.com>
2025-10-23 22:59:55 -0500

    refactor: validación del vector ecopuntos

--- frontend/src/components/map/index.tsx
+++ frontend/src/components/map/index.tsx
@@ -2,7 +2,7 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
 import type { Ecopunto } from '../../interfaces/ecopunto'
 
-function Map({ className, ecopuntos }: { className?: string, ecopuntos: Ecopunto[] }) {
+function Map({ className, ecopuntos = [] }: { className?: string, ecopuntos?: Ecopunto[] }) {
     const center: [number, number] = [6.2710241, -75.55652]
     return (
         <div className={className}>
@@ -11,7 +11,7 @@ function Map({ className, ecopuntos }: { className?: string, ecopuntos: Ecopunto
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {ecopuntos.map((ecopunto) => (
+                {ecopuntos.length > 0 && ecopuntos.map((ecopunto) => (
                     <Marker key={ecopunto.id} position={[Number(ecopunto.ubicacion.latitud), Number(ecopunto.ubicacion.longitud)]}>
                         <Popup>
                             Ecopunto en: <br />{ecopunto.ubicacion.direccion}.
