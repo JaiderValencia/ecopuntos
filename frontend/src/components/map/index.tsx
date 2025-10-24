56e43b82b9a310102d0d63774fdb5f0d904e4f9d
JaiderValencia <janvame05@gmail.com>
2025-10-23 22:55:10 -0500

    Logro: pongo el punto del ecopunto en el mapa

--- frontend/src/components/map/index.tsx
+++ frontend/src/components/map/index.tsx
@@ -1,19 +1,23 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
+import type { Ecopunto } from '../../interfaces/ecopunto'
 
-function Map({ className }: { className?: string }) {
+function Map({ className, ecopuntos }: { className?: string, ecopuntos: Ecopunto[] }) {
+    const center: [number, number] = [6.2710241, -75.55652]
     return (
         <div className={className}>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ width: '100%', height: '100%' }}>
+            <MapContainer center={center} zoom={13} style={{ width: '100%', height: '100%' }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {ecopuntos.map((ecopunto) => (
+                    <Marker key={ecopunto.id} position={[Number(ecopunto.ubicacion.latitud), Number(ecopunto.ubicacion.longitud)]}>
+                        <Popup>
+                            Ecopunto en: <br />{ecopunto.ubicacion.direccion}.
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     )
